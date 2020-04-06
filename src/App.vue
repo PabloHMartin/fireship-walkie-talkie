bd52b5089cd32988c1742ca79795a4f3d2ac57d1
Pablo H <hermar65@gmail.com>
2020-04-06 18:33:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue Voxer</h1>
+      <p>Hola nueva</p>
+    </header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
